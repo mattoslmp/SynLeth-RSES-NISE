9c2cfe3348380dd729773563cf79dd6ee336fc2e
Leandro de Mattos Pereira <mattoslmp@gmail.com>
2026-07-21 14:47:15 -0300

    Test exact SIFTS residue mapping for M-CSA sites

--- tests/test_structural.py
+++ tests/test_structural.py
@@ -11,7 +11,12 @@ from rses_onco.structural import (
   parse_uniprot_features,
   residues_to_frame,
 )
-from rses_onco.structural_mapping import parse_mcsa_uniprot_residues
+from rses_onco.structural_mapping import (
+  MCSAReferenceResidue,
+  map_mcsa_reference_residues_with_sifts,
+  parse_mcsa_uniprot_residues,
+  sifts_uniprot_mapping,
+)
 
 ROOT = Path(__file__).resolve().parents[1]
 
@@ -61,6 +66,44 @@ def test_mcsa_parser_rejects_pdb_only_numbering() -> None:
   assert rows[0].residue_number == 42
   assert rows[0].annotation_type == "mcsa_catalytic"
   assert rows[0].mapping_status == "exact_uniprot_numbering"
+
+
+def test_sifts_maps_one_exact_pdb_residue_to_uniprot() -> None:
+  xml = b"""<?xml version='1.0' encoding='UTF-8'?>
+  <entry xmlns='http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd'>
+    <entity entityId='A' type='protein'>
+      <segment segId='1abc_A_1_100' start='1' end='100'>
+        <listResidue>
+          <residue dbSource='PDBe' dbCoordSys='PDBe' dbResNum='10' dbResName='ASP'>
+            <crossRefDb dbSource='PDB' dbAccessionId='1abc' dbChainId='A' dbResNum='17' dbResName='ASP'/>
+            <crossRefDb dbSource='UniProt' dbAccessionId='P00001' dbResNum='42' dbResName='D'/>
+          </residue>
+        </listResidue>
+      </segment>
+    </entity>
+  </entry>"""
+  assert sifts_uniprot_mapping(
+    xml,
+    accession="P00001",
+    chain_id="A",
+    pdb_residue_number="17",
+  ) == 42
+  references = [MCSAReferenceResidue(
+    pdb_id="1abc",
+    chain_id="A",
+    pdb_residue_number="17",
+    residue_name="ASP",
+    description="general acid/base",
+  )]
+  rows = map_mcsa_reference_residues_with_sifts(
+    references,
+    {"1abc": xml},
+    "TEST",
+    "P00001",
+  )
+  assert len(rows) == 1
+  assert rows[0].residue_number == 42
+  assert rows[0].mapping_status == "pdb_to_uniprot_sifts_exact"
 
 
 def test_pdbe_parser_uses_only_uniprot_numbering() -> None:
